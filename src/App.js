ccdc0ccbcee3b858388ec412442c5f26446b5313
AbdullahLaham <abed26194@gmail.com>
2022-11-05 11:29:02 +0300

    some changes

--- src/App.js
+++ src/App.js
@@ -6,22 +6,27 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './components/Home';
 import Auth from './components/Auth/Auth';
 import PostDetails from './components/PostDetails/PostDetails';
- function App() {
-  
-  return (
-    <Container maxWidth="lg">
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/posts' element={<Home />} />
-          <Route path='/posts/search' element={<Home />} />
-          <Route path='/posts/:id' element={<PostDetails />} />
-          <Route path='/auth' element={<Auth />} />
+import createCache from '@emotion/cache';
+import { CacheProvider } from '@emotion/react';
+const clientSideEmotionCache = createCache({ key: 'css' });
 
-        </Routes>
-     </BrowserRouter>
-    </Container>
+ function App({ Component, pageProps, emotionCache = clientSideEmotionCache, }) {
+  return (
+    <CacheProvider value={emotionCache} >
+      <Container maxWidth="lg">
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/posts' element={<Home />} />
+            <Route path='/posts/search' element={<Home />} />
+            <Route path='/posts/:id' element={<PostDetails />} />
+            <Route path='/auth' element={<Auth />} />
+
+          </Routes>
+      </BrowserRouter>
+      </Container>
+    </CacheProvider>
      
   );
 }
